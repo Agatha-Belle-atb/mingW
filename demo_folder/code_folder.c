6429a2fb9fa2a40eee9b7ab3890756f08741cead
Agatha-Belle-atb <hoaithumath369@gmail.com>
2026-04-30 20:20:37 +0700

    add folder

---
+++ demo_folder/code_folder.c
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a,b;
+	cin >> a >> b;
+	if(a == 0){
+		if(b == 0){
+			cout << "WOW";
+		}
+		else{
+			cout << "NO";
+		}
+	}
+	
+	return 0;
+}
